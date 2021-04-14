fe0f68dc4b8d828480d396306c7f3c3f66c612b5
robmikh <rob.mikh@outlook.com>
2021-04-14 16:53:57 -0700

    update to 0.8

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     windows::build!(
-        windows::ui::{
+        Windows::UI::{
             Color, Colors,
         },
     );
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use bindings::windows::ui::Colors;
+use bindings::Windows::UI::Colors;
 
 fn main() -> windows::Result<()> {
     windows::initialize_sta()?;
 
-    let red = Colors::red()?;
+    let red = Colors::Red()?;
     println!("Red: {:?}", red);
 
     Ok(())
